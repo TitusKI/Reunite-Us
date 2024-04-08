2af77c08df84371fb84ab8e070b505a42fc819af
TitusKI <kidumazie@gmail.com>
2024-04-08 18:24:31 +0300

    Constants and Colors

--- lib/views/common/values/constant.dart
+++ lib/views/common/values/constant.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: constant_identifier_names
+
 class AppConstant {
   static const String STORAGE_DEVICE_OPEN_FIRST_TIME = 'device_first_open';
   static const String STORAGE_USER_TOKEN_KEY = 'user_token_key';
