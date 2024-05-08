7e616db87161a0b27aa70e4ff1081ec82040c9b7
TitusKI <kidumazie@gmail.com>
2024-05-08 18:34:50 +0300

    Sign in Bloc state management

--- lib/bloc/sign_in/sign_in_bloc.dart
+++ lib/bloc/sign_in/sign_in_bloc.dart
@@ -1,15 +1,55 @@
 import 'package:afalagi/bloc/shared_event.dart';
+import 'package:afalagi/repository/user_repository.dart';
 import 'package:afalagi/routes/export.dart';
 part 'sign_in_event.dart';
 part 'sign_in_state.dart';
 
 class SignInBloc extends Bloc<SharedEvent, SignInState> {
-  SignInBloc() : super(const SignInState()) {
+  final UserRepository _repository;
+  SignInBloc(this._repository) : super(const SignInState()) {
     on<EmailEvent>(_emailEvent);
     on<PasswordEvent>(_passwordEvent);
     on<SignInReset>(_resetToInitial);
+    on<SignInSubmitEvent>(_signInSubmitEvent);
+    on<GoogleSignInEvent>(_googleSignInEvent);
     // on<TogglePasswordVisibility>(togglePasswordVisibility);
   }
+  Future<void> _signInSubmitEvent(
+      SignInSubmitEvent event, Emitter<SignInState> emit) async {
+    emit(state.copyWith(isSignInLoading: true));
+    try {
+      await _repository.signin(event.email, event.password);
+      emit(state.copyWith(
+        isSignInLoading: false,
+        signInSuccess: true,
+        signInFailure: "",
+      ));
+    } catch (e) {
+      emit(state.copyWith(
+          isSignInLoading: false,
+          signInFailure: e.toString(),
+          signInSuccess: false));
+    }
+  }
+
+  Future<void> _googleSignInEvent(
+      GoogleSignInEvent event, Emitter<SignInState> emit) async {
+    emit(state.copyWith(isGoogleSignInLoading: true));
+    try {
+      await _repository.signInWithGoogle();
+      emit(state.copyWith(
+        isGoogleSignInLoading: false,
+        isGoogleSignInSuccess: true,
+        googleSignInFailure: "",
+      ));
+    } catch (e) {
+      emit(state.copyWith(
+          isGoogleSignInLoading: false,
+          googleSignInFailure: e.toString(),
+          isGoogleSignInSuccess: false));
+    }
+  }
+
   void _resetToInitial(SignInReset event, Emitter<SignInState> emit) {
     emit(SignInState.initial());
   }
--- lib/bloc/sign_in/sign_in_state.dart
+++ lib/bloc/sign_in/sign_in_state.dart
@@ -5,8 +5,20 @@ class SignInState {
   final String password;
   final bool obscurePassword;
   final IconData iconPassword;
+  final bool isSignInLoading;
+  final bool signInSuccess;
+  final String? signInFailure;
+  final bool isGoogleSignInLoading;
+  final bool isGoogleSignInSuccess;
+  final String? googleSignInFailure;
 
   const SignInState({
+    this.isSignInLoading = false,
+    this.signInSuccess = false,
+    this.signInFailure = "",
+    this.isGoogleSignInLoading = false,
+    this.isGoogleSignInSuccess = false,
+    this.googleSignInFailure = "",
     this.email = "",
     this.password = "",
     this.obscurePassword = true,
@@ -16,18 +28,37 @@ class SignInState {
       : email = "",
         password = "",
         obscurePassword = true,
+        isSignInLoading = false,
+        signInSuccess = false,
+        signInFailure = null,
+        isGoogleSignInLoading = false,
+        isGoogleSignInSuccess = false,
+        googleSignInFailure = null,
         iconPassword = Icons.remove_red_eye_rounded;
-  SignInState copyWith({
-    String? email,
-    String? password,
-    bool? obscurePassword,
-    IconData? iconPassword,
-  }) {
+  SignInState copyWith(
+      {String? email,
+      String? password,
+      bool? obscurePassword,
+      IconData? iconPassword,
+      bool? signInSuccess,
+      bool? isSignInLoading,
+      String? signInFailure,
+      bool? isGoogleSignInLoading,
+      bool? isGoogleSignInSuccess,
+      String? googleSignInFailure}) {
     return SignInState(
       email: email ?? this.email,
       password: password ?? this.password,
       obscurePassword: obscurePassword ?? this.obscurePassword,
       iconPassword: iconPassword ?? this.iconPassword,
+      isSignInLoading: isSignInLoading ?? this.isSignInLoading,
+      signInFailure: signInFailure ?? this.signInFailure,
+      signInSuccess: signInSuccess ?? this.signInSuccess,
+      isGoogleSignInLoading:
+          isGoogleSignInLoading ?? this.isGoogleSignInLoading,
+      googleSignInFailure: googleSignInFailure ?? this.googleSignInFailure,
+      isGoogleSignInSuccess:
+          isGoogleSignInSuccess ?? this.isGoogleSignInSuccess,
     );
   }
 }
