e59bb5aa5e3967eff8747df736dd14ba6169ce07
TitusKI <kidumazie@gmail.com>
2024-06-08 18:51:11 +0300

    Generic State

---
+++ lib/bloc/generic_state.dart
@@ -0,0 +1,31 @@
+import 'package:equatable/equatable.dart';
+
+class GenericState<T> extends Equatable {
+  final bool isLoading;
+  final bool isSuccess;
+  final String? failure;
+
+  final T? data;
+  const GenericState({
+    this.isLoading = false,
+    this.isSuccess = false,
+    this.data,
+    this.failure = "",
+  });
+  GenericState copyWith(
+      {bool? isLoading, bool? isSuccess, String? failure, T? data}) {
+    return GenericState(
+      isLoading: isLoading ?? this.isLoading,
+      isSuccess: isSuccess ?? this.isSuccess,
+      failure: failure ?? this.failure,
+      data: data ?? this.data,
+    );
+  }
+
+  @override
+  List<Object> get props => [
+        isLoading,
+        isSuccess,
+        failure!,
+      ];
+}
