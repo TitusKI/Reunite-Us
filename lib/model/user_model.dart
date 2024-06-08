b534cad5893a3db13ae9afbc5f12b56986e608ad
TitusKI <kidumazie@gmail.com>
2024-06-08 18:42:13 +0300

    Missing Person Model

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -1,31 +1,32 @@
-// lib/models/user_model.dart
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
 
-import 'package:equatable/equatable.dart';
-
-class User extends Equatable {
+class User {
   final String? email;
   final String? password;
   final String? confirmPassword;
+  const User({this.email, this.password, this.confirmPassword});
 
-  const User({
-    this.email,
-    this.password,
-    this.confirmPassword,
-  });
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'email': email,
+      'password': password,
+      'passwordConfirm': confirmPassword,
+    };
+  }
 
-  // Create a copyWith method for easy updates
-  User copyWith({
-    String? email,
-    String? password,
-    String? confirmPassword,
-  }) {
+  factory User.fromMap(Map<String, dynamic> map) {
     return User(
-      email: email ?? this.email,
-      password: password ?? this.password,
-      confirmPassword: confirmPassword ?? this.confirmPassword,
+      email: map['email'] != null ? map['email'] as String : null,
+      password: map['password'] != null ? map['password'] as String : null,
+      confirmPassword: map['passwordConfirm'] != null
+          ? map['passwordConfirm'] as String
+          : null,
     );
   }
 
-  @override
-  List<Object?> get props => [email, password, confirmPassword];
+  String toJson() => json.encode(toMap());
+
+  factory User.fromJson(String source) =>
+      User.fromMap(json.decode(source) as Map<String, dynamic>);
 }
