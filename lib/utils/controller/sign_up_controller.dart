9c120c1eb1a60d0810cce501b1bf3f5ef8813767
TitusKI <kidumazie@gmail.com>
2024-07-03 03:44:25 +0300

    sign_up validator

--- lib/utils/controller/sign_up_controller.dart
+++ lib/utils/controller/sign_up_controller.dart
@@ -1,97 +1,106 @@
-import 'package:afalagi/bloc/sign_up/sign_up_bloc.dart';
-import 'package:afalagi/bloc/sign_up/sign_up_event.dart';
-import 'package:afalagi/bloc/sign_up/sign_up_state.dart';
-import 'package:afalagi/views/common/widgets/flutter_toast.dart';
+// ignore_for_file: unused_local_variable
+
+// import 'package:afalagi/bloc/sign_up/sign_up_bloc.dart';
+// import 'package:afalagi/bloc/sign_up/sign_up_event.dart';
+// import 'package:afalagi/bloc/sign_up/sign_up_state.dart';
+// import 'package:afalagi/views/common/widgets/flutter_toast.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:intl_phone_number_input/intl_phone_number_input.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:intl_phone_number_input/intl_phone_number_input.dart';
 import 'package:regexpattern/regexpattern.dart';
 
 class SignUpController {
-  final BuildContext context;
-  const SignUpController(this.context);
+  // final BuildContext context;
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
+  // SignUpController(this.context);
 
-  void handleEmailSignUp() async {
-    final state = context.read<SignUpBloc>().state;
+  String? handleEmailSignUp(String fieldName, String value) {
+    switch (fieldName) {
+      case 'email':
+        if (value.isEmpty) {
+          return "Email can't be empty";
+        }
+        if (!value.isEmail()) {
+          return "Please Enter a Valid Email Address";
+        }
+        break;
+      case 'password':
+        if (value.isEmpty) {
+          return "Password can't be empty ";
+        }
+        break;
+      case 'repassword':
+        if (value.isEmpty) {
+          return "Your password confirmation is wrong";
+        }
+        if (value == passwordController.text) {
+          return "The Password doesn't match the previous password";
+        }
+        break;
 
-    String email = state.email;
-    String password = state.password;
-    String repassword = state.repassword;
-
-    if (email.isEmpty) {
-      toastInfo(msg: "Email can not be empty");
-      return;
-    } else if (!email.isEmail()) {
-      toastInfo(msg: "Please Enter a Valid Email Address");
-    } else if (password.isEmpty) {
-      toastInfo(msg: "Password can not be empty");
-      return;
-    } else if (repassword.isEmpty) {
-      toastInfo(msg: "Your password confirmation is wrong");
-      return;
-    } else if (repassword != password) {
-      toastInfo(msg: "The Password doesn't match the previous password");
-      return;
-    } else {
-      try {
-        context.read<SignUpBloc>().add(SignUpLoadingEvent());
-        toastInfo(msg: "Registe user Api integration");
-        Navigator.of(context).pushNamed('/sign_up_verification');
-      } catch (e) {
-        context.read<SignUpBloc>().add(SignUpFailureEvent(error: e.toString()));
-      }
+      default:
+        return null;
     }
+    {
+      // try {
+      //   context.read<SignUpBloc>().add(SignUpLoadingEvent());
+      //   toastInfo(msg: "Registe user Api integration");
+      //   Navigator.of(context).pushNamed('/sign_up_verification');
+      // } catch (e) {
+      //   context.read<SignUpBloc>().add(SignUpFailureEvent(error: e.toString()));
+      // }
+    }
+    return null;
   }
 
   void handleProfileBuild() async {
-    final state = context.read<SignUpBloc>().state;
+    //   final state = context.read<SignUpBloc>().state;
 
-    String firstName = state.firstName;
-    String middleName = state.middleName;
-    String lastName = state.lastName;
-    String location = state.location;
-    PhoneNumber? phoneNumber = state.phoneNumber;
-    // String? phoneNumber = state.phoneNumber;
-    String dateOfBirth = state.dateOfBirth;
-    if (state is GenderSelectionState) {
-      String? gender = state.selectedGender;
-      if (gender.isEmpty) {
-        toastInfo(msg: "gender cannot be empty");
-        return;
-      }
-    }
+    //   String firstName = state.firstName;
+    //   String middleName = state.middleName;
+    //   String lastName = state.lastName;
 
-    if (firstName.isEmpty) {
-      toastInfo(msg: "first name can not be empty");
-      return;
-    }
-    if (middleName.isEmpty) {
-      toastInfo(msg: "middle name can not be empty");
-      return;
-    }
-    if (lastName.isEmpty) {
-      toastInfo(msg: "last name can not be empty");
-      return;
-    }
-    if (location.isEmpty) {
-      toastInfo(msg: "location can not be empty");
-      return;
-    }
-    if (phoneNumber.toString().isEmpty) {
-      toastInfo(msg: "phone number can not be empty");
-      return;
-    }
-    if (dateOfBirth.isEmpty) {
-      toastInfo(msg: "date of birth can not be empty");
-      return;
-    }
-    try {
-      toastInfo(msg: "Profile Created Succesfully");
-      Navigator.of(context)
-          .pushNamedAndRemoveUntil('/sign_in', (Route<dynamic> route) => false);
-      return;
-    } catch (e) {
-      print(e.toString());
-    }
+    //   PhoneNumber? phoneNumber = state.phoneNumber;
+    //   // String? phoneNumber = state.phoneNumber;
+    //   String dateOfBirth = state.dateOfBirth;
+    //   if (state is GenderSelectionState) {
+    //     String? gender = state.selectedGender;
+    //     if (gender.isEmpty) {
+    //       "gender cannot be empty";
+    //     }
+    //   }
+
+    //   if (firstName.isEmpty) {
+    //     "first name can not be empty";
+    //   }
+    //   if (middleName.isEmpty) {
+    //     "middle name can not be empty";
+    //   }
+    //   if (lastName.isEmpty) {
+    //     "last name can not be empty";
+    //   }
+    //   // if (location.isEmpty) {
+    //   //   toastInfo(msg: "location can not be empty");
+    //   //   return;
+    //   // }
+    //   if (phoneNumber.toString().isEmpty) {
+    //     "phone number can not be empty";
+    //     return;
+    //   }
+    //   if (dateOfBirth.isEmpty) {
+    //     "date of birth can not be empty";
+    //   }
+    //   try {
+    //     toastInfo(msg: "Profile Created Succesfully");
+    //     Navigator.of(context)
+    //         .pushNamedAndRemoveUntil('/sign_in', (Route<dynamic> route) => false);
+    //     return;
+    //   } catch (e) {
+    //     print(e.toString());
+    //   }
   }
 }
