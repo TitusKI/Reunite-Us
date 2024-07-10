fd4e86e3f51805db56802bf481d25e4e0cd1b4a5
TitusKI <kidumazie@gmail.com>
2024-07-10 10:59:00 +0300

    First Time Logged In shared reference

--- lib/core/services/storage_services.dart
+++ lib/core/services/storage_services.dart
@@ -12,9 +12,9 @@ class StorageService {
     return await _prefs.setBool(key, value);
   }
 
-  Future<bool> setString(String key, String value) async {
-    return await _prefs.setString(key, value);
-  }
+  // Future<bool> setString(String key, String value) async {
+  //   return await _prefs.setString(key, value);
+  // }
 
   bool getDeviceFirstOpen() {
     return _prefs.getBool(AppConstant.STORAGE_DEVICE_OPEN_FIRST_TIME) ?? false;
