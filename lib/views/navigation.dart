5a92723b1a182e7ad790a1b5e10e1733dae4353b
TitusKI <kidumazie@gmail.com>
2024-07-28 18:32:41 +0300

    Main Navigation Screen

--- lib/views/navigation.dart
+++ lib/views/navigation.dart
@@ -1,9 +1,11 @@
 import 'package:afalagi/bloc/bottom_navigation/bottom_navigation_bloc.dart';
+import 'package:afalagi/routes/names.dart';
 import 'package:afalagi/views/chat_screen/screens/chat.dart';
 import 'package:afalagi/views/common/values/colors.dart';
 import 'package:afalagi/views/common/widgets/common_widgets.dart';
 import 'package:afalagi/views/found_screen/screens/found.dart';
 import 'package:afalagi/views/home_screen/screens/home.dart';
+import 'package:afalagi/views/drawer_screen/screens/drawer.dart';
 import 'package:afalagi/views/report_screen/screens/report.dart';
 import 'package:afalagi/views/search_screen/screens/search.dart';
 import 'package:convex_bottom_bar/convex_bottom_bar.dart';
@@ -11,7 +13,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -21,11 +24,40 @@ class MyHomePage extends StatelessWidget {
         Widget body;
         String appBarTitle;
         List<Widget> appBarActions = [];
+        Widget? leading;
+        Widget? drawer;
 
         switch (state.navIndex) {
           case 0:
             body = const Home();
-            appBarTitle = "Home";
+            appBarTitle = "Reunite-Us";
+
+            appBarActions = [
+              IconButton(
+                  style: const ButtonStyle(
+                    foregroundColor:
+                        WidgetStatePropertyAll(AppColors.primaryBackground),
+                    // backgroundColor:
+                    //     WidgetStatePropertyAll(AppColors.primaryBackground),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(AppRoutes.ADD_REPORT);
+                  },
+                  icon: const Icon(Icons.add_circle_outline_outlined))
+            ];
+            leading = IconButton(
+              icon: Image.asset(
+                'assets/logo/logo1.png',
+                // color: AppColors.primaryBackground,
+                filterQuality: FilterQuality.high,
+                scale: 0.5,
+              ),
+              onPressed: () {
+                _scaffoldKey.currentState?.openDrawer();
+              },
+            );
+
+            drawer = const AppDrawer();
             break;
           case 1:
             body = const SearchMissing();
@@ -56,16 +88,28 @@ class MyHomePage extends StatelessWidget {
             appBarTitle = "Afalagi";
         }
         return Scaffold(
-          appBar: buildAppBarLarge(appBarTitle, actions: appBarActions),
+          key: _scaffoldKey,
+          drawer: drawer,
+          appBar: buildAppBarLarge(appBarTitle,
+              actions: appBarActions, leading: leading),
           bottomNavigationBar: ConvexAppBar(
             style: TabStyle.react,
             backgroundColor: AppColors.secondaryColor,
-            items: const [
-              TabItem(icon: Icons.home_rounded, title: "Home"),
-              TabItem(icon: Icons.search_rounded, title: "Search"),
-              TabItem(icon: Icons.live_help_rounded, title: "Report"),
-              TabItem(icon: Icons.chat_rounded, title: "Chat"),
-              TabItem(icon: Icons.emoji_events_rounded, title: "Found")
+            items: [
+              TabItem(
+                icon: Image.asset('assets/icons/home.png'),
+                title: "Home",
+              ),
+              TabItem(
+                  icon: Image.asset('assets/icons/searchMissing.png'),
+                  title: "Search"),
+              TabItem(
+                  icon: Image.asset('assets/icons/report.png'),
+                  title: "Report"),
+              TabItem(
+                  icon: Image.asset('assets/icons/chat.png'), title: "Chat"),
+              TabItem(
+                  icon: Image.asset('assets/icons/success.png'), title: "Found")
             ],
             initialActiveIndex: state.navIndex,
             onTap: (int i) {
