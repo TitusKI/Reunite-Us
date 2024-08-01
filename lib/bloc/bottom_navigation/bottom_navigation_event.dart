40ec6d89361ad4ebeb7f5c2e0211c3f4840ef5ae
TitusKI <kidumazie@gmail.com>
2024-08-01 09:33:47 +0300

    Cleaning Up

--- lib/bloc/bottom_navigation/bottom_navigation_event.dart
+++
@@ -1,9 +0,0 @@
-part of 'bottom_navigation_bloc.dart';
-
-class BottomNavigationEvent extends Equatable {
-  final int navIndex;
-  const BottomNavigationEvent(this.navIndex);
-
-  @override
-  List<Object> get props => [];
-}
--- lib/bloc/welcome/welcome_state.dart
+++
@@ -1,7 +0,0 @@
-part of 'welcome_bloc.dart';
-
-
-class WelcomeState {
-  int page;
-  WelcomeState({this.page = 0});
-}
