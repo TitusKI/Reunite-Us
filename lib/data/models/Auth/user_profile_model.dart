bd936e32f63fd28e6a48ae9b89d7dc71e638b2d3
TitusKI <kidumazie@gmail.com>
2024-08-03 10:11:47 +0300

    Auth Models

---
+++ lib/data/models/Auth/user_profile_model.dart
@@ -0,0 +1,35 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'user_profile_model.g.dart';
+
+@JsonSerializable()
+class UserProfile {
+  final String? firstName;
+  final String? middleName;
+
+  final String? lastName;
+  final String? phoneNumber;
+  final String? state;
+  final String? city;
+  final String? country;
+  final String? birthDate;
+  final String? profilePicture;
+  final String? gender;
+  final String? email;
+
+  UserProfile({
+    this.email,
+    this.phoneNumber,
+    required this.firstName,
+    this.middleName,
+    required this.lastName,
+    required this.birthDate,
+    this.state,
+    this.city,
+    required this.country,
+    required this.gender,
+    this.profilePicture,
+  });
+  factory UserProfile.fromJson(Map<String, dynamic> json) =>
+      _$UserProfileFromJson(json);
+  Map<String, dynamic> toJson() => _$UserProfileToJson(this);
+}
