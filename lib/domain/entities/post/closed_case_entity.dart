9df347445af3830b7dbff80a2b3f8215558c7e18
TitusKI <kidumazie@gmail.com>
2024-08-09 10:44:17 +0300

    Missing Person Entities

---
+++ lib/domain/entities/post/closed_case_entity.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
+class ClosedCaseEntity {
+  final Future<String?>? id;
+  final String? foundCondition;
+  final String? foundDate;
+  final String? foundLocation;
+  final String? foundThrough;
+  final String? description;
+  ClosedCaseEntity({
+    this.id,
+    this.foundCondition,
+    this.foundDate,
+    this.foundLocation,
+    this.foundThrough,
+    this.description,
+  });
+}
