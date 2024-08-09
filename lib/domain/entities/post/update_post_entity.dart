a10cabee2f9530530b838cb471b1302b0f5eb5e6
TitusKI <kidumazie@gmail.com>
2024-08-09 10:46:52 +0300

    Missing Person Update  Entities

---
+++ lib/domain/entities/post/update_post_entity.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class UpdatePostEntity {
+  final String? id;
+  final Map<String, dynamic>? updates;
+  UpdatePostEntity({
+    this.id,
+    this.updates,
+  });
+}
