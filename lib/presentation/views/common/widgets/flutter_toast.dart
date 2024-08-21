ad80f6deebab6ce7bf1bacf325a3e06e413f823d
TitusKI <kidumazie@gmail.com>
2024-08-21 11:46:09 +0300

    Presentation common  widgets

---
+++ lib/presentation/views/common/widgets/flutter_toast.dart
@@ -0,0 +1,21 @@
+import 'package:afalagi/config/theme/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+toastInfo({
+  required String msg,
+  Color backgroundColor = Colors.white,
+  Color textColor = AppColors.accentColor,
+  ToastGravity gravity = ToastGravity.TOP,
+}) {
+  return Fluttertoast.showToast(
+    msg: msg,
+    toastLength: Toast.LENGTH_SHORT,
+    gravity: gravity,
+    timeInSecForIosWeb: 5,
+    backgroundColor: backgroundColor,
+    textColor: textColor,
+    fontSize: 16.sp,
+  );
+}
---
+++ lib/presentation/views/common/widgets/gener_field.dart
@@ -0,0 +1,87 @@
+import 'package:afalagi/presentation/bloc/user/profile/create_profile/create_profile_bloc.dart';
+import 'package:afalagi/presentation/bloc/user/profile/create_profile/create_profile_event.dart';
+import 'package:afalagi/presentation/bloc/post/report_form/report_form_bloc.dart';
+import 'package:afalagi/presentation/bloc/post/report_form/report_form_event.dart';
+import 'package:afalagi/core/util/controller/enum_utility.dart';
+import 'package:afalagi/core/util/controller/enums.dart';
+import 'package:afalagi/presentation/views/common/widgets/build_textfield.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+Widget dropDownField<T extends Enum>(
+    {String? selected,
+    List<T>? dropDown,
+    BuildContext? context,
+    String? hintText,
+    required String keyName}) {
+  return MyTextField(
+    readOnly: true,
+    prefixIcon: const Icon(Icons.person),
+    suffixIcon: DropdownButton<String>(
+      icon: const Icon(Icons.arrow_drop_down),
+      iconSize: 24,
+      elevation: 16,
+      underline: Container(
+        height: 0,
+        color: Colors.transparent,
+      ),
+      items: dropDown!.map<DropdownMenuItem<String>>((T value) {
+        return DropdownMenuItem(
+          value: value.toShortString(),
+          child: Text(value.toShortString()),
+        );
+      }).toList(),
+      onChanged: (String? value) {
+        switch (keyName) {
+          case 'genderR':
+            final gender = stringToGender(value!);
+            context!
+                .read<ReportFormBloc>()
+                .add(ReportFormEvent(onGender: gender));
+            break;
+          case 'gender':
+            final gender = stringToGender(value!);
+            context!.read<CreateProfileBloc>().add(GenderEvent(gender));
+            break;
+          case 'hairColor':
+            final hairColor = stringToHairColor(value!);
+            context!
+                .read<ReportFormBloc>()
+                .add(ReportFormEvent(onHairColor: hairColor));
+            break;
+          case 'educationalLevel':
+            final educationLevel = stringToEducationalLevel(value!);
+            context!
+                .read<ReportFormBloc>()
+                .add(ReportFormEvent(onEducationalLevel: educationLevel));
+            break;
+          case 'maritalStatus':
+            final maritalStatus = stringToMaritalStatus(value!);
+            context!
+                .read<ReportFormBloc>()
+                .add(ReportFormEvent(onMaritalStatus: maritalStatus));
+            break;
+          case 'skinColor':
+            final skinColor = stringToSkinColor(value!);
+            context!
+                .read<ReportFormBloc>()
+                .add(ReportFormEvent(onSkinColor: skinColor));
+            break;
+          default:
+            return;
+        }
+      },
+    ),
+    controller: TextEditingController(text: selected),
+    hintText: hintText!,
+    // dropDown == ["Male", "Female"] ? "Select Gender" : "Has Disability?",
+    obscureText: false,
+    keyboardType: TextInputType.none,
+    validator: (validate) {
+      if (validate == null || validate.isEmpty) {
+        return dropDown == Gender.values ? "Please select a gender" : null;
+      }
+      return null;
+    },
+  );
+}
