401a45f10c519a59e203e594f0b5a6efaa6335ef
TitusKI <kidumazie@gmail.com>
2024-08-28 11:57:53 +0300

    Success Story Usecases

--- lib/domain/usecases/user/update_user_profile.dart
+++ lib/domain/usecases/user/update_user_profile.dart
@@ -1,5 +1,5 @@
 import 'package:afalagi/core/usecase/usecase.dart';
-import 'package:afalagi/data/models/Auth/user_profile_model.dart';
+import 'package:afalagi/data/models/user/user_profile_model.dart';
 import 'package:afalagi/domain/repository/user/user_repository.dart';
 import 'package:afalagi/injection_container.dart';
 
