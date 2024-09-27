623f3c91f0f0011903d30e73121fd29d22b0a614
TitusKI <kidumazie@gmail.com>
2024-09-27 12:14:28 +0300

    Add Initialization in Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,37 @@
 // ignore_for_file: must_be_immutable
 
-import 'package:afalagi/bloc/theme_cubit/theme_cubit.dart';
-import 'package:afalagi/routes/pages.dart';
-import 'package:afalagi/global.dart';
+import 'package:afalagi/presentation/bloc/common/theme_cubit/theme_cubit.dart';
+import 'package:afalagi/config/routes/pages.dart';
+import 'package:afalagi/injection_container.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized(); // Ensure binding is initialized
-  await Global.init(); // Wait for initialization
+  await initializeDependencies(); // Wait for initialization
   runApp(MyApp()); // Run the app after initialization
 }
 
 class MyApp extends StatelessWidget {
-  AppPages appPages = AppPages(Global.userRepository);
+  AppPages appPages = AppPages();
   MyApp({super.key});
-
-  // This widget is the root of this application.
 
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-        providers: [...appPages.allBlocProvider(context)],
-        child: ScreenUtilInit(
-            // designSize: const Size(375, 812),
-            builder: (context, child) => BlocBuilder<ThemeCubit, ThemeData>(
-                  builder: (context, theme) {
-                    return MaterialApp(
-                      theme: theme,
-
-                      // theme for both
-
-                      // theme: ThemeData(
-                      //   colorScheme: const ColorScheme.highContrastLight(),
-                      //   textTheme:
-                      //       GoogleFonts.soraTextTheme(Theme.of(context).textTheme),
-                      // ),
-                      debugShowCheckedModeBanner: false,
-                      onGenerateRoute: appPages.generateRouteSettings,
-                    );
-                  },
-                )));
+      providers: [...appPages.allBlocProvider(context)],
+      child: ScreenUtilInit(
+        // designSize: const Size(375, 812),
+        builder: (context, child) => BlocBuilder<ThemeCubit, ThemeData>(
+          builder: (context, theme) {
+            return MaterialApp(
+              theme: theme,
+              debugShowCheckedModeBanner: false,
+              onGenerateRoute: appPages.generateRouteSettings,
+            );
+          },
+        ),
+      ),
+    );
   }
 }
